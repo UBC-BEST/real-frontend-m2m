aa0a6b6cf1864a806f38dfac89f33b1adae46ec5
Scurvy <48632814+DrakonNX@users.noreply.github.com>
2022-04-02 11:37:22 -0700

    setting up src

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+/**
+ * main app stuff lol
+ */
+
+import React from 'react';
+import { GameEngine } from "react-native-game-engine";
+import Constants from './Constants';
+
+
+export default function App() {
+  return (
+    <View style={styles.canvas}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  canvas: {
+    flex: 1,
+    backgroundColor: "#000000",
+    alignItems: "center", 
+    justifyContent: "center",
+  },
+});
