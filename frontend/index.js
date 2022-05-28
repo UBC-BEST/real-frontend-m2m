035dde17b81c35a57f4a1759aabc06595785087e
Scurvy <48632814+DrakonNX@users.noreply.github.com>
2022-05-28 12:48:57 -0700

    framework for HitCircle

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './frontend/src/App';
-import {name as appName} from './frontend/app.json';
+import App from './src/App';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,52 @@
  import React, { useRef } from 'react';
  import { GameEngine} from 'react-native-game-engine';
  import Constants from './Constants';
+
+ import HitCircle from './Components/GameObjects/HitCircle';
  
  export default function App() {
-     const engine = useRef(null); 
- 
-     return (
-         <View style={styles.canvas}>
-             <GameEngine
-                 ref = {engine}
+  //constants
+  const BoardSize = Constants.MAX_HEIGHT * Constants.MAX_WIDTH;
+  const engine = useRef(null); 
+
+  //random positions function
+  const randomPositions = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  
+  return (
+    <View style={styles.canvas}>
+      <GameEngine
+        ref = {engine}
                  
-                 style={{
-                     width: Constants.MAX_WIDTH,
-                     height: Constants.MAX_HEIGHT,
-                     flex: null,
-                     backgroundColor: "white",
-                 }}
-             />
-         </View>
-     );
+        style={{
+          width: Constants.MAX_WIDTH,
+          height: Constants.MAX_HEIGHT,
+          flex: null,
+          backgroundColor: "white",
+        }}
+
+        entities={{
+          //styling for hit circle
+          HitCircle: {
+            position: [
+              randomPositions(0, Constants.MAX_HEIGHT - 1),
+              randomPositions(0, Constants.MAX_WIDTH - 1),
+            ],
+            size: 10,
+            renderer: <Food />,
+          },
+        }}
+      />
+    </View>
+  );
  }
  
  const styles = StyleSheet.create({
-     canvas: {
-         flex: 1,
-         backgroundColor: "#000000",
-         alignItems: "center",
-         justifyContent: "center",
-     }
+  canvas: {
+    flex: 1,
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
+  }
  });
