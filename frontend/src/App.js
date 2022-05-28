6f24199a9ffea68f6443bef9bc5c9ccc0fe73d06
Scurvy <48632814+DrakonNX@users.noreply.github.com>
2022-05-28 12:22:02 -0700

    more setting up files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-/**
- * main app stuff lol
- */
-
-import React from 'react';
-import { GameEngine } from "react-native-game-engine";
-import Constants from './Constants';
-
-
-export default function App() {
-  return (
-    <View style={styles.canvas}>
-      
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  canvas: {
-    flex: 1,
-    backgroundColor: "#000000",
-    alignItems: "center", 
-    justifyContent: "center",
-  },
-});
+ import React, { useRef } from 'react';
+ import { GameEngine} from 'react-native-game-engine';
+ import Constants from './Constants';
+ 
+ export default function App() {
+     const engine = useRef(null); 
+ 
+     return (
+         <View style={styles.canvas}>
+             <GameEngine
+                 ref = {engine}
+                 
+                 style={{
+                     width: Constants.MAX_WIDTH,
+                     height: Constants.MAX_HEIGHT,
+                     flex: null,
+                     backgroundColor: "white",
+                 }}
+             />
+         </View>
+     );
+ }
+ 
+ const styles = StyleSheet.create({
+     canvas: {
+         flex: 1,
+         backgroundColor: "#000000",
+         alignItems: "center",
+         justifyContent: "center",
+     }
+ });
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from '../app.json';
+import App from './frontend/src/App';
+import {name as appName} from './frontend/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
